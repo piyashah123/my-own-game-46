508b66eb1bb6795b5aff83ebf8f30f65430fbb2d
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2022-02-27 13:00:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function preload(){
+trackImg= loadIamge("track.png");
+
+}
+function setup(){
+player=createSprite(200,200,45,45);
+
+}
+function draw(){
+
+}
+function playerControl(){
+
+}
+function spawnGarbage(){
+
+}
+function touchingGarbage(){
+
+}
